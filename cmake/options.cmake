a99c4be0179ed87fe8cb755dd78b9911ea27f976
Alex Wegener <alexander_wegener1998@yahoo.de>
2026-03-21 19:58:17 -0400

    changed gitignore

---
+++ cmake/options.cmake
@@ -0,0 +1,4 @@
+option(BUILD_WITH_FRANKA  "Compile with libfranka for real robot" OFF)
+option(BUILD_WITH_MUJOCO  "Compile with MuJoCo simulation" ON)
+option(BUILD_WITH_TESTS   "Build test targets" ON)
+option(BUILD_STREAMER "Build video streamer" ON)
